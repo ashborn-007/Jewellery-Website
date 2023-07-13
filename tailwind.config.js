b97078f9e0460c930c9451f04ee352d840e0bc54
ashborn-007 <91942765+ashborn-007@users.noreply.github.com>
2023-07-13 16:13:16 +0530

    Uploaded Code to Repository - Srijan

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      'mullish': ['"Fontdiner Swanky"', "sans-serif"],
+      'comorant': ['Cormorant',"sans-serif"]
+      
+    },
+  },
+  plugins: [],
+};
